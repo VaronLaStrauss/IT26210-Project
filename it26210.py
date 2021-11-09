de76b0ac5bbbbae8983abdb0cd18f1bb779a87f7
Viany Manuel <jeangmanuel@gmail.com>
2021-11-09 17:18:13 +0800

    Lint changes

--- it26210.py
+++ it26210.py
@@ -1,23 +1,30 @@
-import urllib.parse
-import requests
 from operator import itemgetter
 import json
+import requests
+from urllib import parse
 
-main_api = "https://open.mapquestapi.com/directions/v2/alternateroutes?"
-key = "zTPMdKRWp9kgYVPLVPtGUGfNHhu2dmUY"
+MAIN_API = "https://open.mapquestapi.com/directions/v2/alternateroutes?"
+KEY = "zTPMdKRWp9kgYVPLVPtGUGfNHhu2dmUY"
 
 def test_api_status_matcher() -> bool:
     status = 402
-    assert api_status_matcher(status) == False
+    assert api_status_matcher(status) is False
     status = 611
-    assert api_status_matcher(status) == False
+    assert api_status_matcher(status) is False
     status = 0
-    assert api_status_matcher(status) == True
+    assert api_status_matcher(status) is True
     status = "any"
-    assert api_status_matcher(status) == False
+    assert api_status_matcher(status) is False
 
 def get_route(origin: str, destination: str, number_of_routes: int) -> any:
-    url = main_api + urllib.parse.urlencode({"key": key, "from": origin, "to": destination, "maxRoutes": number_of_routes})
+    url = MAIN_API + parse.urlencode(
+            {
+                "key": KEY, 
+                "from": origin, 
+                "to": destination, 
+                "maxRoutes": number_of_routes
+            }
+        )
     print(url)
     json_data = requests.get(url).json()
     json_data = json.dumps(json_data)
@@ -25,7 +32,7 @@ def get_route(origin: str, destination: str, number_of_routes: int) -> any:
     return json_data
 
 def api_status_matcher(status: int) -> bool:
-    shouldContinue = False
+    should_continue = False
     match status:
         case 402:
             print("**********************************************")
@@ -38,7 +45,7 @@ def api_status_matcher(status: int) -> bool:
         case 0:
             print("API Status: 0 - A successful route call.\n")
             print("=============================================")
-            shouldContinue = True
+            should_continue = True
         case _:
             print("************************************************************************")
             print("For Staus Code: " + 
@@ -46,13 +53,13 @@ def api_status_matcher(status: int) -> bool:
                 "; Refer to: https://developer.mapquest.com/documentation/directions-api/status-codes")
             print("************************************************************************")
     print()
-    return shouldContinue
+    return should_continue
 
-def print_route(json_data: any, distUnit: float, gasUnit: float, inner = False) -> None:
+def print_route(json_data: any, distUnit: float, gas_unit: float, inner = False) -> None:
     route = itemgetter('route')(json_data)
-    hasTollRoad, hasBridge, hasTunnel, \
-        hasCountryCross, formattedTime, \
-        distance, fuelUsed = itemgetter(
+    has_toll, has_bridge, has_tunnel, \
+        has_country_cross, formatted_time, \
+        distance, fuel_used = itemgetter(
             'hasTollRoad',
             'hasBridge',
             'hasTunnel',
@@ -62,16 +69,16 @@ def print_route(json_data: any, distUnit: float, gasUnit: float, inner = False) 
             'fuelUsed'
         )(route)
 
-    if hasTollRoad:
+    if has_toll:
         print("The road that you will go through will have a toll gate so ready your cash.")
-    if hasBridge:
+    if has_bridge:
         print("The road that you will go through will have a bridge so be safe when driving.")
-    if hasTunnel:
+    if has_tunnel:
         print("The road that you will go through will have a tunnel so make sure your lights are working.")
-    if hasCountryCross:
+    if has_country_cross:
         print("You need to go to another country to reach your destination.")
 
-    print("Trip Duration: " + formattedTime)
+    print("Trip Duration: " + formatted_time)
     print(
         "Distance: " + 
         str("{:.2f}".format(distance)) + 
@@ -79,8 +86,8 @@ def print_route(json_data: any, distUnit: float, gasUnit: float, inner = False) 
 
     print(
         "Fuel Used: " + 
-        str("{:.2f}".format(fuelUsed * gasUnit)) + 
-        (" gallon(s)" if gasUnit == 1 else " Liter(s)"))
+        str("{:.2f}".format(fuel_used * gas_unit)) + 
+        (" gallon(s)" if gas_unit == 1 else " Liter(s)"))
     print()
 
     for maneuver in route["legs"][0]["maneuvers"]:
@@ -97,15 +104,15 @@ def print_route(json_data: any, distUnit: float, gasUnit: float, inner = False) 
     print("=============================================\n")
 
     if "alternateRoutes" in route:
-            for alternate in route["alternateRoutes"]:
-                print("Alternate Route: \n")
-                print_route(alternate, distUnit, gasUnit, True)
+        for alternate in route["alternateRoutes"]:
+            print("Alternate Route: \n")
+            print_route(alternate, distUnit, gas_unit, True)
     elif not inner:
         print("No alternate routes found.")
 
 def main():
-    shouldQuit = False
-    while not shouldQuit:
+    should_quit = False
+    while not should_quit:
         loc1 = ""
         loc2 = ""
         while loc1 == "":
@@ -113,17 +120,17 @@ def main():
         while loc2 == "":
             loc2 = input("Destination: ")
     
-        distUnit = 1 if input("Use kilometers for distance? (Y/n): ").lower() == "n" else 1.6
-        gasUnit = 1 if input("Use Liters for gasoline? (Y/n): ").lower() == "n" else 3.78541
+        dist_unit = 1 if input("Use kilometers for distance? (Y/n): ").lower() == "n" else 1.6
+        gas_unit = 1 if input("Use Liters for gasoline? (Y/n): ").lower() == "n" else 3.78541
 
-        if("" in [loc1, loc2]): 
+        if "" in [loc1, loc2]: 
             print("Please enter a value for both locations")
             continue
 
         number_of_routes = 1  
         try:
             route_str = (input("Number of routes (1): "))
-            if(route_str == ""):
+            if route_str == "":
                 number_of_routes = 1
             number_of_routes = int(route_str)
         except ValueError:
@@ -133,13 +140,13 @@ def main():
         json_data = get_route(loc1, loc2, number_of_routes)
         json_status = json_data["info"]["statuscode"]
 
-        if(api_status_matcher(json_status)):
+        if api_status_matcher(json_status):
             print("Directions from " + (loc1) + " to " + (loc2))
             print()
-            print_route(json_data, distUnit, gasUnit)
+            print_route(json_data, dist_unit, gas_unit)
 
-        userContinue = input("Continue? (Y/n): ").lower()
-        shouldQuit = userContinue == "n" or userContinue == "no"
+        user_continue = input("Continue? (Y/n): ").lower()
+        should_quit = user_continue in ["n", "no"]
 
 if __name__ == "__main__":
     main()
